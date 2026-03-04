e4e5d77692ddc09f2703068498ca37492c7f275b
waditariq <tariqalt300@gmail.com>
2026-03-04 12:51:25 -0800

    feat3-sr2 Commit #2 - restaurants_service was not added in last commit for some reason, it was especially crucial

--- backend/app/services/restaurants_service.py
+++ backend/app/services/restaurants_service.py
@@ -1,4 +1,7 @@
-# Import the repository class
+''' FULL DISCLAIMER
+I need to have a chat with the team about how we will do tags.
+As of right now, everyone is busy, but I have to continue working regardless.
+So for now, SR2 will start like SR1, not using the CSV file, but working with mock data (for now) '''# Import the repository class
 # The service will depend on the repository to retrieve data
 from app.repositories.restaurants_repository import RestaurantsRepository
 
@@ -18,4 +21,33 @@ class RestaurantsService:
         restaurants = self.repo.get_all() # Call the repository to retrieve raw data
 
         # For now, we return it directly, until filtering and all is added
+        return restaurants
+    
+    ''' SR2 STUFF - 
+    Here we will implement some search and filter functionality 
+    and there should be a unit test in the same commit to test this method '''    
+    def search_restaurants(self, q=None, restaurant_id=None, is_open=None, tag=None):
+        
+        restaurants = self.repo.get_all() # Like above, call the repository to retrieve raw data
+
+        # Here we match the restaurant to the argument passed in the endpoint
+        if restaurant_id is not None: 
+            restaurants = [r for r in restaurants if r.get("restaurantId") == restaurant_id] # So it filters out whatever IDs are irrelevant to the one passed in the endpoint
+
+        ''' So I'm putting this here for the future. 
+        I remember our team spoke about restaurants opening and closing,
+        but we haven't agreed on putting those values yet. From SR1, the repository sets isOpen= True for now '''
+        if is_open is not None:
+            restaurants = [r for r in restaurants if r.get("isOpen") == is_open]
+
+        # Tag filter (mockable in tests; real tags can come later)
+        if tag is not None: # ie we DO have a tag of some kind
+            tag_norm = str(tag).strip().lower() # "Normalize" it by removing whitespace and making it lowercase
+            if tag_norm == "": # So we have a tag value, but it's just an empty string, which is not valid, then...
+                raise ValueError("tag cannot be empty") # Raise an error! Amazing
+            restaurants = [
+                r for r in restaurants # So add the restaurant to the list IF
+                if tag_norm in str(r.get("tags", "")).lower() # The restaurant has a "tags" field, and the normalized tag is found within it (case-insensitive)
+            ]
+
         return restaurants
