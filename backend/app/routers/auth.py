7913798629f853653208a2b458d8b875503cd6d3
Talon Lusk <txlon5@student.ubc.ca>
2026-03-14 15:23:00 -0700

    Update login process to return user object after valid login

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -10,8 +10,8 @@ router = APIRouter(prefix="/auth", tags=["auth"])
 @router.post("/login", response_model=Token)
 def login(form_data: OAuth2PasswordRequestForm = Depends()):
     # Verify user login is valid or throw exception
-    login_user(form_data.username, form_data.password)
+    user = login_user(form_data.username, form_data.password)
     # Pass in username and create authorized JWT token
-    token = create_token({"sub": form_data.username})
+    token = create_token({"sub": user.id})
     # Return valid token to user logging in
     return Token(access_token=token, token_type="bearer")
--- backend/app/services/users_service.py
+++ backend/app/services/users_service.py
@@ -115,7 +115,7 @@ def delete_user(user_id: str) -> None:
         raise HTTPException(status_code=404, detail=f"User '{user_id}' not found")
     save_all(new_users)
 
-def login_user (email:str, password: str) -> None:
+def login_user (email:str, password: str) -> User:
     """
     Login the user matching the given userid
     Raises 422 if email or password is invalid
@@ -127,13 +127,18 @@ def login_user (email:str, password: str) -> None:
     if not UserValidator.is_valid_password(password):
         raise HTTPException(status_code=422, detail="Password must at minimum 8 characters, have 1 capital and 1 special character.")
 
+    # Fetch user
+    user = get_user_by_email(email)
+
     # Fetch hash password
-    hash_password = get_user_by_email(email).password
+    hash_password = user.password
     
     # Validate password related to user
     if not (PasswordHandler.verify_password(password, hash_password)):
         # If invalid raise issue for invalid credentials
         raise HTTPException(status_code=401, detail="Invalid credentials.")
+    
+    return user
 
 
 # Helper Functions
