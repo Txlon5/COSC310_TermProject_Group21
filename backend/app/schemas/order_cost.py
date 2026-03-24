3c35b737217bf38d06833b3a67ebafe3814d0054
Omarion Forde <omarionf13@gmail.com>
2026-03-24 15:24:17 -0700

    Refactored, took out comments

--- backend/app/schemas/order_cost.py
+++ backend/app/schemas/order_cost.py
@@ -5,15 +5,12 @@ from typing import List
 class OrderItemRequest(BaseModel):
     # The ID of the menu item the user wants to order
     item_id: str = Field(..., min_length=1)
-
-    # How many of that item the user wants
     quantity: int = Field(..., ge=1)
 
 
 class SubtotalRequest(BaseModel):
     # The restaurant the order belongs to
     restaurant_id: str = Field(..., min_length=1)
-
     # The selected items in the order/cart
     items: List[OrderItemRequest] = Field(..., min_length=1)
 
