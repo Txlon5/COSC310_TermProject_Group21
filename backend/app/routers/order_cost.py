01e28f62e49b957ae3d909ad93245e84b36fff3e
Omarion Forde <omarionf13@gmail.com>
2026-03-24 22:44:43 -0700

    Added endpoint, last commit was mistake

--- backend/app/routers/order_cost.py
+++ backend/app/routers/order_cost.py
@@ -1,16 +1,20 @@
-from fastapi import APIRouter,HTTPException
-from app.schemas.order_cost import SubtotalRequest, SubtotalResponse
+from fastapi import APIRouter
+from app.schemas.order_cost import SubtotalRequest, SubtotalResponse,OrderCostRequest,OrderCostResponse
 from app.services.menu_service import fetch_menu_by_restaurant_id
-from app.services.order_cost_service import calculate_order_subtotal
+from app.services.order_cost_service import calculate_order_subtotal, calculate_order_cost
 
 router = APIRouter(prefix="/order-cost", tags=["Order Cost"])
 
 
 @router.post("/subtotal", response_model=SubtotalResponse)
 def calculate_subtotal_endpoint(payload: SubtotalRequest):
-    if payload.delivery_method is not None:
-        if payload.delivery_method not in ["delivery", "pickup"]:
-            raise HTTPException(status_code=400,detail="delivery_method must be either 'delivery' or 'pickup'.")
+    
      #SR1 endpoint:Calculates the subtotal of the selected order items.
     menu_items = fetch_menu_by_restaurant_id(payload.restaurant_id)
     return calculate_order_subtotal(payload, menu_items)
+
+# SR2
+@router.post("/calculate", response_model=OrderCostResponse)
+def calculate_order_cost_endpoint(payload: OrderCostRequest):
+    menu_items = fetch_menu_by_restaurant_id(payload.restaurant_id)
+    return calculate_order_cost(payload, menu_items)
