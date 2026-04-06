e67d02493d9ac3ea24bf0586f9db13a0d4b4fa60
txlon5 <txlon5@student.ubc.ca>
2026-04-05 20:54:04 -0700

    Implement action token valid function

--- backend/app/services/action_token_service.py
+++ backend/app/services/action_token_service.py
@@ -1,8 +1,10 @@
 import uuid
-from datetime import datetime
+from datetime import datetime, timedelta
 from fastapi import HTTPException
 from app.repositories.auth_repository import load_all, save_all
 from app.schemas.auth import ActionToken, ActionTokenType
+
+TOKEN_EXPIRY_MINUTES = 30
 
 
 def create_action_token(token_type: ActionTokenType, user_id: str) -> ActionToken:
@@ -17,17 +19,18 @@ def create_action_token(token_type: ActionTokenType, user_id: str) -> ActionToke
     new_token = ActionToken(
         id=str(uuid.uuid4()),
         user_id=user_id,
-        type=ActionTokenType.reset,
+        type=token_type,
         created_at=datetime.now(),
         used=False,
     )
 
     # Save action token and return it
-    tokens.append(new_token.model_dump())
+    tokens.append(new_token.model_dump(mode="json"))
     save_all(tokens)
     return new_token
 
-def get_action_token_by_id(token_id: str):
+
+def get_action_token_by_id(token_id: str) -> ActionToken:
     # Fetch token values
     tokens = load_all()
 
@@ -38,3 +41,18 @@ def get_action_token_by_id(token_id: str):
     raise HTTPException(status_code=404, detail=f"Token '{token_id}' not found")
 
 
+def is_action_token_valid(token_id: str) -> bool:
+    # Fetch token associated with token_id
+    token = get_action_token_by_id(token_id)
+
+    # Check if token used
+    if not token.used:
+        # Calculate time expiry from created time
+        expiry = token.created_at + timedelta(minutes=TOKEN_EXPIRY_MINUTES)
+        # Check if token is expired
+        if datetime.now() < expiry:
+            # Token valid
+            return True
+
+    # Token invalid
+    return False
